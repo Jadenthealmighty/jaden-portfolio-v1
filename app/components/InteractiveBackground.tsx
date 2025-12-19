d19986611f835b935adef07f475d7992ad65b494
jadenal-aidroos <jjtortoise@icloud.com>
2025-12-18 20:18:04 -0500

    Getting edges to follow animation

--- app/components/InteractiveBackground.tsx
+++ app/components/InteractiveBackground.tsx
@@ -5,6 +5,8 @@ import { useEffect, useRef } from "react";
 type Node = {
   x0: number;
   y0: number;
+  xt: number;
+  yt: number;
 };
 
 export default function InteractiveBackground() {
@@ -41,7 +43,7 @@ export default function InteractiveBackground() {
       for (let y = 0; y < window.innerHeight + spacing; y += vSpacing) {
         for (let x = 0; x < window.innerWidth + spacing; x += spacing) {
           const offset = Math.round(y / vSpacing) % 2 ? spacing / 2 : 0;
-          nodes.push({ x0: x + offset, y0: y });
+          nodes.push({ x0: x + offset, y0: y, xt: x + offset, yt: y});
         }
       }
     }
@@ -59,24 +61,7 @@ export default function InteractiveBackground() {
       const nx = 1 / Math.sqrt(2);
       const ny = -1 / Math.sqrt(2);
 
-      // draw edges
-      ctx.strokeStyle = "rgba(150, 170, 255, 0.08)";
-      ctx.lineWidth = 1;
 
-      for (let i = 0; i < nodes.length; i++) {
-        for (let j = i + 1; j < nodes.length; j++) {
-          const dx0 = nodes[i].x0 - nodes[j].x0;
-          const dy0 = nodes[i].y0 - nodes[j].y0;
-          const dist0 = Math.sqrt(dx0 * dx0 + dy0 * dy0);
-
-          if (dist0 < spacing * 1.1) {
-            ctx.beginPath();
-            ctx.moveTo(nodes[i].x0, nodes[i].y0);
-            ctx.lineTo(nodes[j].x0, nodes[j].y0);
-            ctx.stroke();
-          }
-        }
-      }
 
       // draw nodes
       for (const n of nodes) {
@@ -97,6 +82,8 @@ export default function InteractiveBackground() {
 
         const x = n.x0 + wx;
         const y = n.y0 + wy;
+        n.xt = x;
+        n.yt = y;
 
         const radius = 1.6 + mouseT * 2.8;
         const alpha = 0.15 + mouseT * 0.6 + waveAmp * 0.25;
@@ -105,6 +92,25 @@ export default function InteractiveBackground() {
         ctx.arc(x, y, radius, 0, Math.PI * 2);
         ctx.fillStyle = `rgba(117, 250, 246, ${alpha})`;
         ctx.fill();
+      }
+
+      // draw edges
+      ctx.strokeStyle = "rgba(150, 170, 255, 0.08)";
+      ctx.lineWidth = 1;
+
+      for (let i = 0; i < nodes.length; i++) {
+        for (let j = i + 1; j < nodes.length; j++) {
+          const dx0 = nodes[i].x0 - nodes[j].x0;
+          const dy0 = nodes[i].y0 - nodes[j].y0;
+          const dist0 = Math.sqrt(dx0 * dx0 + dy0 * dy0);
+
+          if (dist0 < spacing * 1.1) {
+            ctx.beginPath();
+            ctx.moveTo(nodes[i].xt, nodes[i].yt);
+            ctx.lineTo(nodes[j].xt, nodes[j].yt);
+            ctx.stroke();
+          }
+        }
       }
 
       requestAnimationFrame(draw);

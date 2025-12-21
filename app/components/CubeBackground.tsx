f1f35abeba61c6565bdf3decb84d0f742b6498c5
jadenal-aidroos <jjtortoise@icloud.com>
2025-12-21 03:47:37 -0500

    Quality of life stuff

--- app/components/CubeBackground.tsx
+++ app/components/CubeBackground.tsx
@@ -117,7 +117,7 @@ type Vec3 = {
         
         z = (z + 4) * 400;
         const oZ = z;
-        lensY = centerY - scrollY;
+        lensY = centerY - scrollY * 0.9;
         const dFromCent = Math.sqrt((oX - lensX) ** 2 +  (oY - lensY) ** 2);
         if (dFromCent >= tempR){
             ctx.beginPath();
@@ -169,11 +169,11 @@ type Vec3 = {
       let focString = "Focal length: " + (Math.round(tempF) / 100);
       ctx.font = "14px Arial";
       ctx.fillStyle = "white";
-      ctx.fillText(focString, 0.45 * canvas.width, 0.1 * canvas.height - scrollY);
+      ctx.fillText(focString, 0.45 * canvas.clientWidth, 0.1 * canvas.clientHeight - scrollY);
       let rString = "Lens radius: " + (Math.round(tempR) / 100);
       ctx.font = "14px Arial";
       ctx.fillStyle = "white";
-      ctx.fillText(rString, 0.45 * canvas.width, 0.1 * canvas.height + 20 - scrollY);
+      ctx.fillText(rString, 0.45 * canvas.clientWidth, 0.1 * canvas.clientHeight + 20 - scrollY);
       ctx.globalAlpha = 1;
 
 

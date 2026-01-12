ed927ecc5194f050f41feb0e035add0c0dee8987
jadenal-aidroos <jjtortoise@icloud.com>
2026-01-12 07:04:38 -0500

    Adding Skills4

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -4,6 +4,34 @@ export default function SkillsPage() {
         <main className="relative z-10 px-12 pt-[40vh] pb-20 max-w-4xl lg:max-w-5xl xl:max-w-6xl">
         </main>
 
+        <h2 className= "mb-8 text-2xl font-bold text-[#75faf6]">
+            About This Page
+        </h2>
+          <p className="mb-4">
+        This is a Machine Learning unsupervised model running solely in your browser. 
+        Every bit of matrix operations has been optimized to run in the TypeScript file, 
+        being run 100% on your computer. The model only sees where your mouse was 5 seconds 
+        ago and calculates its best prediction(in <span style={{ color: '#75FAF6', marginBottom: '2.5rem' }}>blue</span>) of where your mouse currently is(in white).
+          </p>
+          <p className="mb-10">
+        The background is actually a direct projection of the bias matrix that runs 
+        the majority of the program's functionality. Look at how the entries change over time!
+
+        </p>
+        <h1 className="mb-8 text-5xl font-semibold tracking-tighter">
+        Skills
+        </h1>
+        <h2 className= "mb-8 text-2xl font-bold text-[#75faf6]">
+        Computer Programming
+      </h2>
+      <p className="mb-6">
+      I have been coding for the majority of my life; as a result, I am proficient in 
+      Python, Java, and C++ programming languages, with Python being my primary language. 
+      I have intermediate knowledge of MATLAB and C. Most recently, I’ve been using a lot of 
+      C to optimize my machine learning model and write MCU code for robotics. I am also very 
+      familiar with JavaScript and, by extension, Typescript. And I have a working but basic 
+      knowledge of HTML and CSS.</p>
+
         {/* Stuff here */}
         <section>
         <h1 className="text-5xl font-bold mb-6">Projects</h1>
@@ -39,63 +67,48 @@ export default function SkillsPage() {
         <img src="/images/linux-logo.webp" alt="Linux" className="language-logo" />
       </div>
     </div>
+    <p className="mb-6"></p>
 
-  
+    <p className="mb-10">
+      I’ve done work with my own Linux (Ubuntu) server management, and so I’m very 
+      familiar with bash, file systems, and Linux maintenance. In terms of operating 
+      systems, most of my workflow is in macOS, but I am very familiar with Windows 
+      and the Microsoft Office applications.
+
+      </p>
     
-          <p className="mb-10">
-            I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-             I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-             I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-        </p>
-        <h2 className= "mb-8 text-2xl font-bold text-[#75faf6]">
-            Project 1
-        </h2>
-          <p className="mb-10">
-            I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-             I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-             I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-        </p>
-        <h2 className= "mb-8 text-2xl font-bold text-[#75faf6]">
-            Project 1
-        </h2>
-          <p className="mb-10">
-            I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-             I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-             I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-        </p>
-        <h2 className= "mb-8 text-2xl font-bold text-[#75faf6]">
-            Project 1
-        </h2>
-          <p className="mb-10">
-            I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-             I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-             I have a strong background in computational modelling
-             and a passion for problem solving. In 2024 I competed in the US Physics Olympiad known otherwise as the
-             Team USA Physics Competition. 
-        </p>
-        
+      <h2 className= "mb-8 text-2xl font-bold text-[#75faf6]">
+        Math and Physics
+      </h2>
+      <p className="mb-10">
+      I competed in the USAPhO(USA Physics Olympiad), Putnam Competition, and CAML 
+      (California Math League). Earning the ranks of top 100 in the USA and top 5 
+      in California for the USAPhO and CAML, respectively. Recently, much of my math 
+      has been in combinatorics, graph theory, and stochastic calculus.
+      </p>
+      <h2 className= "mb-8 text-2xl font-bold text-[#75faf6]">
+        Engineering
+      </h2>
+      <p className="mb-10">
+      I have a working knowledge of CAD, and I regularly use both SolidWorks and Autodesk 
+      Fusion. For the last four years, I have been in different robotics teams working on 
+      both programming and manufacturing. In 2024, my deployed code was used in the FRC 
+      World Championships. Through this, I’ve gained extensive experience soldering, using 
+      power tools, and 3D printing.
+      </p>
+      <h2 className= "mb-8 text-2xl font-bold text-[#75faf6]">
+        Presentational
+      </h2>
+      <p className="mb-4">
+      I have strong public speaking skills through competing in parliamentary debate 
+      and earning the Eagle Scout rank in Boy Scouts, and I have had plenty of experience 
+      in PowerPoint and Google Slides.
+      </p>
+      <p className="mb-4">
+One of my biggest personal hobbies is videography and video editing. I have had upwards 
+of 7 years of experience in Premiere Pro and After Effects, and Blender. I currently also 
+work as a contract videographer for the UofT newspaper, The Varsity.
+      </p> 
 
         </section>
         </section>
